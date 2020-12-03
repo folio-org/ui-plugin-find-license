f0c0ff4f74e1cd7ad7fd04657ec1372c2bd26958
Peter <boehm@hebis.uni-frankfurt.de>
2020-12-03 18:18:00 +0100

    ERM-1237

--- src/View.js
+++ src/View.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { noop } from 'lodash';
-import { FormattedDate, FormattedMessage } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 
 import {
   MultiColumnList,
@@ -71,7 +71,7 @@ export default class View extends React.Component {
   formatter = {
     type: ({ type }) => type?.label,
     status: ({ status }) => status?.label,
-    startDate: license => (license.startDate ? <FormattedUTCDate value={license.startDate} /> : ''),
+    startDate: ({ startDate }) => (startDate ? <FormattedUTCDate value={startDate} /> : ''),
     endDate: license => <LicenseEndDate license={license} />,
   }
 
